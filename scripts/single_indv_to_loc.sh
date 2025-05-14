bdecc0923ac5d03c09e494906ba4307c897f5ede
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2025-05-14 08:58:32 -0700

    Create single_indv_to_loc.sh

---
+++ scripts/single_indv_to_loc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=256x44               # Partition/queue to run on
+#SBATCH --time=2-00:00:00                # Max time for job to run
+#SBATCH --job-name=filtere_gene           # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=igpedraz@ucsc.edu    # Where to send mail
+#SBATCH --ntasks=1                       # Number of tasks to run
+#SBATCH --cpus-per-task=8                # Number of CPU cores to use per task
+#SBATCH --nodes=1                        # Number of nodes to use
+#SBATCH --mem=15G                        # Ammount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out            # Standard output and error log
+#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
+
+
