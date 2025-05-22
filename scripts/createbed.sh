e6684df3ce86e5114a7ae85e09dd0b0217f2b2aa
pedrazaitzelg <155600640+pedrazaitzelg@users.noreply.github.com>
2025-05-22 15:31:36 -0700

    Create createbed.sh

---
+++ scripts/createbed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=128x24               # Partition/queue to run on
+#SBATCH --time=1-00:00:00                # Max time for job to run
+#SBATCH --job-name=allele_filter                # Name for job (shows when running squeue)
+#SBATCH --mail-type=ALL                  # Mail events(NONE,BEGIN,END,FAIL,ALL)
+#SBATCH --mail-user=igpedraz@ucsc.edu    # Where to send mail
+#SBATCH --ntasks=1                       # Number of tasks to run
+#SBATCH --cpus-per-task=8                # Number of CPU cores to use per task
+#SBATCH --nodes=1                        # Number of nodes to use
+#SBATCH --mem=10G                        # Ammount of RAM to allocate for the task
+#SBATCH --output=slurm_%j.out            # Standard output and error log
+#SBATCH --error=slurm_%j.err             # Standard output and error log
+#SBATCH --no-requeue                     # don't requeue the job upon NODE_FAIL
+
+
+awk -v chrom_value="NW_026622808.1" 'BEGIN {OFS="\t"} NR==1 {print "CHROM", $2, $3; next} {print chrom_value, $2, $3}' insr_exons.bed > insr_exons_fix.bed
+
+
